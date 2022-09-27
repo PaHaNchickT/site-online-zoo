fb4af74949d47c25a5708c4a8c13cc3cfe17e2ee
Pavel <you@example.com>
2022-09-27 14:13:47 +0300

    fix: fix small errors on main page (14:13 27.09.22)

---
+++ online-zoo/pages/main/script.js
@@ -0,0 +1,9 @@
+// document.querySelector('html').addEventListener('click', function() {
+//     if (document.querySelector('input:focus') !== null && document.querySelector('input:valid') !== null) {
+//         console.log('valid')
+//     }
+// })
+
+document.querySelector('input').addEventListener('change', function() {
+    console.log('test')
+})
