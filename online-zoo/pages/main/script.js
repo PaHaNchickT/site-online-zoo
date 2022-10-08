79f2d77726adae660856e937f7d095092056ebaf
Pavel <you@example.com>
2022-10-09 01:03:49 +0300

    test: make pag prototype (1:03 09.10.22)

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,29 +1,37 @@
-document.querySelector('body').addEventListener("keyup", function (event) {
-    if (document.querySelector('input:focus') !== null && document.querySelector('input:valid') !== null) {
-        document.querySelector('input').style.borderColor = 'green'
-        document.querySelector('input').style.color = 'green'
-        document.querySelector('form > button').style.color = 'green'
-        document.querySelector('form > button').style.borderColor = 'green'
-        console.log('valid')
+const body = document.querySelector('body')
+const input = document.querySelector('input')
+const inputfocus = document.querySelector('input:focus')
+const inputvalid = document.querySelector('input:valid')
+const inputinvalid = document.querySelector('input:invalid')
+const formbutton = document.querySelector('form > button')
+const pagdiv = document.querySelectorAll('.pag > div')
+
+////////////////////////////////////////////////base/////////////////////////////////////////////////////
+
+body.addEventListener("keyup", function (event) {
+    if (inputfocus !== null && inputvalid !== null) {
+        input.style.borderColor = 'green'
+        input.style.color = 'green'
+        formbutton.style.color = 'green'
+        formbutton.style.borderColor = 'green'
     }
-    if (document.querySelector('input:focus') !== null && document.querySelector('input:invalid') !== null) {
-        document.querySelector('input').style.borderColor = 'red'
-        document.querySelector('input').style.color = 'red'
-        document.querySelector('form > button').style.color = 'red'
-        document.querySelector('form > button').style.borderColor = 'red'
-        console.log('invalid')
+    if (inputfocus !== null && inputinvalid !== null) {
+        input.style.borderColor = 'red'
+        input.style.color = 'red'
+        formbutton.style.color = 'red'
+        formbutton.style.borderColor = 'red'
     }
 })
 
-document.querySelectorAll('.pag > div').forEach(e => {
+pagdiv.forEach(e => {
     e.addEventListener('mouseover', function () {
         e.querySelector('.title').style.top = '0'
         e.style.backgroundSize = '110%'
         e.style.backgroundPosition = '-10px 0'
-    }) 
+    })
 })
 
-document.querySelectorAll('.pag > div').forEach(e => {
+pagdiv.forEach(e => {
     e.addEventListener('mouseout', function () {
         e.querySelector('.title').style.top = '50px'
         e.style.backgroundSize = '100.5%'
@@ -31,3 +39,54 @@ document.querySelectorAll('.pag > div').forEach(e => {
     })
 })
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////carousel//////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const pagBox = document.querySelector('.pagBox')
+const btnL = document.querySelector('.btnL')
+const btnR = document.querySelector('.btnR')
+const pagMain = document.querySelector('.pagMain')
+const pagAdd = document.querySelector('.pagAdd')
+
+/////////////////////////////////////////////////core////////////////////////////////////////////////
+// console.log(btnL)
+btnL.addEventListener('click', function (event) {
+    pagBox.childNodes.forEach((e, i) => {
+    })
+    console.log(pagBox.childNodes[1].getBoundingClientRect().left)
+    console.log(pagBox.childNodes[3].getBoundingClientRect().left)
+    if (pagBox.childNodes[1].getBoundingClientRect().left < pagBox.childNodes[3].getBoundingClientRect().left) {
+        pagBox.childNodes[1].style.left = '-1193.6px'
+        pagBox.childNodes[1].style.opacity = '0'
+        pagBox.childNodes[3].style.left = '0'
+        pagBox.childNodes[3].style.opacity = '1'
+        pagBox.childNodes[1].addEventListener('transitionend', function() {
+            pagBox.childNodes[1].style.left = '1193.6px'
+            pagBox.childNodes[3].style.left = '0'
+            console.log(pagBox.childNodes[1].getBoundingClientRect().left)
+            console.log(pagBox.childNodes[3].getBoundingClientRect().left)
+        })
+    } else {
+        pagBox.childNodes[3].style.left = '-1193.6px'
+        pagBox.childNodes[3].style.opacity = '0'
+        pagBox.childNodes[1].style.left = '0'
+        pagBox.childNodes[1].style.opacity = '1'
+        pagBox.childNodes[3].addEventListener('transitionend', function() {
+            pagBox.childNodes[1].style.left = '0'
+            pagBox.childNodes[3].style.left = '1193.6px'
+            console.log(pagBox.childNodes[1].getBoundingClientRect().left)
+            console.log(pagBox.childNodes[3].getBoundingClientRect().left)
+        })
+    }
+})
+
+btnR.addEventListener('click', function (event) {
+    pagMain.style.left = '0'
+    pagAdd.style.left = '1193.6px'
+    pagBox.childNodes.forEach((e, i) => {
+        if (e.className) {
+            console.log(e.getBoundingClientRect().left)
+        }
+    })
+})
