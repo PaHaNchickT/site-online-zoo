715d334c78bf7cacebb91975a2ff5de3cdebdefb
Pavel <you@example.com>
2022-10-09 16:12:45 +0300

    feat: make full working pag core (16:12 09.10.22)

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -44,6 +44,7 @@ pagdiv.forEach(e => {
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const pagBox = document.querySelector('.pagBox')
+const pagEl = document.getElementsByClassName('card1')[0]
 const btnL = document.querySelector('.btnL')
 const btnR = document.querySelector('.btnR')
 const pagMain = document.querySelector('.pagMain')
@@ -55,6 +56,7 @@ const splitter = document.querySelector('.splitter')
 let divCenter = pagBox.childNodes[1]
 let divAdd = pagBox.childNodes[3]
 let isClicked = false
+let n
 btnL.addEventListener('click', function () {
     splitter.style.display = 'block'
     if (isClicked === false) {
@@ -67,7 +69,6 @@ btnL.addEventListener('click', function () {
             divCenter.style.left = '-1193.6px'
         }, 1)
         divCenter.style.opacity = '0'
-
         isClicked = true
     } else {
         divCenter.style.transition = '0s'
@@ -76,10 +77,9 @@ btnL.addEventListener('click', function () {
             divCenter.style.transition = '1s'
             divCenter.style.left = '0'
             divCenter.style.opacity = '1'
-            divAdd.style.opacity = '0'
+            divAdd.style.left = '-1193.6px'
         }, 1)
-        divAdd.style.left = '-1193.6px'
-
+        divAdd.style.opacity = '0'
         isClicked = false
     }
     divCenter.addEventListener('transitionend', function () { splitter.style.display = 'none' })
@@ -111,4 +111,81 @@ btnR.addEventListener('click', function () {
         isClicked = false
     }
     divCenter.addEventListener('transitionend', function () { splitter.style.display = 'none' })
+})
+
+function boxResize() {
+    console.log(pagBox.clientWidth / (pagEl.clientWidth + 34))
+    console.log(pagEl.clientWidth)
+
+    switch (window.screen.width) {
+        case 1600: (function () {
+            if (window.localStorage) {
+                if (!localStorage.getItem('firstLoad')) {
+                    localStorage['firstLoad'] = true;
+                    window.location.reload();
+                }
+                else
+                    localStorage.removeItem('firstLoad');
+            }
+        })()
+            break;
+        case 1000: (function () {
+            if (window.localStorage) {
+                if (!localStorage.getItem('firstLoad')) {
+                    localStorage['firstLoad'] = true;
+                    window.location.reload();
+                }
+                else
+                    localStorage.removeItem('firstLoad');
+            }
+        })()
+            break;
+        case 640: (function () {
+            if (window.localStorage) {
+                if (!localStorage.getItem('firstLoad')) {
+                    localStorage['firstLoad'] = true;
+                    window.location.reload();
+                }
+                else
+                    localStorage.removeItem('firstLoad');
+            }
+        })()
+            break;
+        case 320: (function () {
+            if (window.localStorage) {
+                if (!localStorage.getItem('firstLoad')) {
+                    localStorage['firstLoad'] = true;
+                    window.location.reload();
+                }
+                else
+                    localStorage.removeItem('firstLoad');
+            }
+        })()
+            break;
+    }
+
+    if (window.screen.width >= 1600) {
+        n = Math.floor((pagBox.clientWidth / (pagEl.clientWidth + 34)))
+        divAdd.style.width = `${(pagEl.clientWidth + 34) * n}px`
+        divCenter.style.width = `${(pagEl.clientWidth + 34) * n}px`
+    } else if (window.screen.width >= 1000 && window.screen.width < 1600) {
+        n = Math.floor((pagBox.clientWidth / (pagEl.clientWidth + 4)))
+        divAdd.style.width = `${(pagEl.clientWidth + 4) * n + 46}px`
+        divCenter.style.width = `${(pagEl.clientWidth + 4) * n + 46}px`
+    } else if (window.screen.width >= 640 && window.screen.width < 1000) {
+        n = Math.floor((pagBox.clientWidth / (pagEl.clientWidth + 4)))
+        divAdd.style.width = `${(pagEl.clientWidth + 4) * n + 24}px`
+        divCenter.style.width = `${(pagEl.clientWidth + 4) * n + 24}px`
+    } else if (window.screen.width >= 320 && window.screen.width < 640) {
+        n = Math.floor((pagBox.clientWidth / (pagEl.clientWidth + 4)))
+        console.log(n)
+        divAdd.style.width = `${(pagEl.clientWidth + 4) * n + 4}px`
+        divCenter.style.width = `${(pagEl.clientWidth + 4) * n + 4}px`
+    }
+}
+
+boxResize()
+
+window.addEventListener('resize', function () {
+    boxResize()
 })
