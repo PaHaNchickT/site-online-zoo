f781e77f167cbd26ea35fbc2002c54307316cd77
Pavel <you@example.com>
2022-10-11 05:16:19 +0300

    fix: edit links color on donate page (5:16 11.10.22)

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -60,6 +60,8 @@ burgerSplitter.addEventListener('click', function () {
 })
 
 function burgerOpen() {
+    document.querySelector('.donate').classList.remove('link-off')
+    document.querySelector('.donate > a').style.color = 'black'
     burger.style.display = 'none'
     menu.style.display = 'block'
     by.style.display = 'block'
@@ -79,6 +81,7 @@ function burgerOpen() {
 }
 
 function burgerClose() {
+    document.querySelector('.donate').classList.add('link-off')
     burger.style.display = 'block'
     menu.style.display = 'none'
     by.style.display = 'none'
